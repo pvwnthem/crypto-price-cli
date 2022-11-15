feed69d293e3f7a09aac2d0014e3b2bee43028ff
pvwn <pvwn.them@gmail.com>
2022-11-15 15:20:54 -0500

    get links

--- resolveSymbol.py
+++ resolveSymbol.py
@@ -1,6 +1,11 @@
 import requests
 from bs4 import BeautifulSoup
 
+from difflib import SequenceMatcher
+
+def similar(a, b):
+    return SequenceMatcher(None, a, b).ratio()
+
 res = requests.get('https://coinmarketcap.com/currencies/')
 
 soup = BeautifulSoup(res.text, 'html.parser')
@@ -14,6 +19,11 @@ names = []
 for tr in trs:
     coinsQuery = tr.find_all('p', class_='coin-item-symbol')
     cryptoQuery = tr.find_all('span', class_='crypto-symbol')
+
+    namesQuery = tr.find_all('a', href=True, class_='cmc-link')
+
+    for name in namesQuery:
+        names.append(name["href"])
     for coin in coinsQuery:
         coins.append(coin.text)
     
@@ -21,8 +31,7 @@ for tr in trs:
         coins.append(crypto.text)
     
 
-print(coins)
+for name in names:
+    print(names.index(name))
 
 
-
-for tr in trs:
